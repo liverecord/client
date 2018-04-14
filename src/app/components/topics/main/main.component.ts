93f8a5aa650da8fc29f9e708eb3ea4d0b5dcda6d
Philipp Tkachev <zoonman@gmail.com>
2018-04-14 18:37:29 -0400

    Angular 6 upgrade + password restore. Modified main.component.ts

--- src/app/components/topics/main/main.component.ts
+++ src/app/components/topics/main/main.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {WebSocketService} from '../../../services/ws.service';
+import { Topic } from '../../../models/topic';
 
 @Component({
   selector: 'lr-main',
@@ -8,8 +9,13 @@ import {WebSocketService} from '../../../services/ws.service';
 })
 export class MainComponent implements OnInit {
   public connected: boolean;
+  public topic: Topic;
   constructor(protected webSocketService: WebSocketService) {
     this.connected = false;
+  }
+
+  topicSelected(t) {
+    this.topic = t;
   }
 
   ngOnInit() {
