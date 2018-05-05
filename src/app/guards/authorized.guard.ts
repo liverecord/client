ee71092de363f95cff6043201409ad6197ec4805
Philipp Tkachev <zoonman@gmail.com>
2018-05-04 23:34:59 -0400

    Angular 6 + File upload. New file authorized.guard.ts

---
+++ src/app/guards/authorized.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
+
+
+@Injectable()
+export class AuthorizedGuard implements CanActivate {
+  constructor(private userService: UserService, private router: Router) {}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.userService.isAuthorized()) {
+      return true;
+    } else {
+      this.router.navigate(['users', 'login']);
+      return false;
+    }
+  }
+}
