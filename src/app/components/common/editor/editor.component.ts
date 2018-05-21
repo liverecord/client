57ff5cb6394dee9dc5d27ac793e1367e41fbe81b
Philipp Tkachev <zoonman@gmail.com>
2018-05-20 23:35:01 -0400

    Angular 6 + File upload. Modified editor.component.ts

--- src/app/components/common/editor/editor.component.ts
+++ src/app/components/common/editor/editor.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit, Input, Output, EventEmitter, AfterViewInit, ViewChild} from '@angular/core';
 import {ContenteditableDirective} from './contenteditable.directive';
+import { UploadFile } from '../../../models/file';
+import { FrameType, WebSocketService } from '../../../services/ws.service';
 
 @Component({
   selector: 'lr-editor',
@@ -16,6 +18,7 @@ export class EditorComponent implements OnInit, AfterViewInit {
   private contentEditable: ContenteditableDirective;
 
   private previousHtml = '';
+  uploads: UploadFile[];
 
   toolbar = [
     {active: true,  command: 'bold', fa: 'bold', hotKey: 'Ctrl+B'},
@@ -74,7 +77,11 @@ export class EditorComponent implements OnInit, AfterViewInit {
     }
   }
 
-  constructor() {  }
+  constructor(
+    private webSocketService: WebSocketService,
+  ) {
+    this.uploads = [];
+  }
 
   format(command, $event) {
     $event.preventDefault();
@@ -83,7 +90,6 @@ export class EditorComponent implements OnInit, AfterViewInit {
   }
 
   refreshState() {
-
     if (document.activeElement === this.contentEditable.getNativeElement()) {
       this.toolbar.map(function(item) {
         const pn = {
@@ -105,10 +111,6 @@ export class EditorComponent implements OnInit, AfterViewInit {
     }
   }
 
-  dragend(event) {
-    console.log(event);
-  }
-
   update() {
     if (this.previousHtml !== this.html) {
       this.htmlChange.emit(this.html);
@@ -126,10 +128,51 @@ export class EditorComponent implements OnInit, AfterViewInit {
       this.refreshState();
     }, 1000);
     this.contentEditable.moveCaret();
-
     this.refreshState();
+    this.webSocketService.subscribe(frame => {
+      switch (frame.type) {
+        case FrameType.CancelUpload:
+          const cancelledUpload = <UploadFile>UploadFile.fromObject(frame.data);
+          const cancelledUploadFileIndex = this.getUploadIndex(cancelledUpload);
+          if (cancelledUploadFileIndex >= 0) {
+            this.uploads.splice(cancelledUploadFileIndex, 1);
+          }
+          break;
+        case FrameType.File:
+          const f = <UploadFile>UploadFile.fromObject(frame.data);
+          f.calculateProgressPercent();
+          const uploadFileIndex = this.getUploadIndex(f);
+          if (uploadFileIndex < 0) {
+            this.uploads.push(f);
+          }
+          if (f.path && f.path.length > 0 && f.uploaded === f.size) {
+            this.focusEditor();
+            let html = '';
+            if (f.type.startsWith('image/', 0)) {
+              html = createImage('http://localhost:8000' + f.path, f.name);
+            } else if (f.type.startsWith('video/', 0)) {
+              html = createVideo('http://localhost:8000' + f.path, f.name);
+            } else {
+              html = createLink('http://localhost:8000' + f.path, f.name);
+            }
+            document.execCommand('insertHTML', false,  html + ' ');
+            this.uploads.splice(uploadFileIndex, 1);
+          }
+          break;
+      }});
   }
 
+  private getUploadIndex(f: UploadFile): number {
+    let uploadFileIndex = -1;
+    this.uploads.map((v, i) => {
+      if (v.name === f.name && v.size === f.size && v.type === f.type) {
+        v.uploaded = f.uploaded;
+        v.calculateProgressPercent();
+        uploadFileIndex = i;
+      }
+    });
+    return uploadFileIndex;
+  }
   ngAfterViewInit() {
     this.refreshState();
   }
@@ -140,4 +183,86 @@ export class EditorComponent implements OnInit, AfterViewInit {
       .replace('Shift+', '⇧');
   }
 
+  onDrop($event) {
+    console.log('topic.detail', $event);
+    if ($event.dataTransfer.files.length > 0) {
+      $event.preventDefault();
+      for (let i = 0; i < $event.dataTransfer.files.length; i++) {
+        const fr = new FileReader();
+        const file = $event.dataTransfer.files[i];
+        fr.addEventListener('load', (e) => {
+          console.log(e, fr.result, file, JSON.stringify(file));
+          this.webSocketService.next({
+            type: FrameType.File,
+            data: {
+              name: file.name,
+              size: file.size,
+              type: file.type,
+              lastModifiedDate: file.lastModifiedDate,
+            }
+          });
+          this.webSocketService.next(fr.result);
+        });
+        fr.addEventListener('onabort', (a) => {
+          console.log('onabort', a);
+        });
+        fr.addEventListener('progress', (p) => {
+          console.log('progress', p);
+        });
+        fr.addEventListener('onloadend', (e) => {
+          console.log('load', e);
+        });
+        fr.readAsArrayBuffer(file);
+      }
+    }
+  }
+
+  cancelUpload(index) {
+    const file = this.uploads[index];
+    this.webSocketService.next({
+      type: FrameType.CancelUpload,
+      data: {
+        name: file.name,
+        size: file.size,
+        type: file.type,
+        lastModifiedDate: file.lastModifiedDate,
+      }
+    });
+    this.uploads.splice(index, 1);
+  }
+
+  focusEditor() {
+    const editor = document.querySelector('.editor');
+    if (editor instanceof HTMLElement) {
+      editor.focus();
+    }
+  }
+}
+
+
+function createImage(src, alt) {
+  const ie = document.createElement('img');
+  ie.setAttribute('src', src);
+  ie.setAttribute('alt', alt);
+  return ie.outerHTML;
+}
+
+function createVideo(src, alt) {
+  const ie = document.createElement('video');
+  ie.setAttribute('src', src);
+  ie.setAttribute('alt', alt);
+  ie.setAttribute('controls', '');
+  ie.setAttribute('preload', 'metadata');
+  ie.setAttribute('width', '640');
+  ie.setAttribute('height', '480');
+  return ie.outerHTML;
+}
+
+function createLink(href, anchor) {
+  const ie = document.createElement('a');
+  ie.setAttribute('link', href);
+  ie.setAttribute('target', '_blank');
+  ie.setAttribute('rel', 'nofollow');
+  ie.innerText = anchor;
+  return ie.outerHTML;
 }
