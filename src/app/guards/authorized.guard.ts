142f015363e40622e7822f0ba1a8f8974acd5e78
Philipp Tkachev <zoonman@gmail.com>
2018-06-10 15:34:07 -0400

    Fixing bugs. Modified authorized.guard.ts

--- src/app/guards/authorized.guard.ts
+++ src/app/guards/authorized.guard.ts
@@ -3,7 +3,6 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from
 import { Observable } from 'rxjs';
 import { UserService } from '../services/user.service';
 
-
 @Injectable()
 export class AuthorizedGuard implements CanActivate {
   constructor(private userService: UserService, private router: Router) {}
@@ -12,9 +11,9 @@ export class AuthorizedGuard implements CanActivate {
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     if (this.userService.isAuthorized()) {
       return true;
-    } else {
-      this.router.navigate(['users', 'login']);
-      return false;
     }
+    this.userService.redirectUrl = state.url;
+    this.router.navigate(['users', 'login']);
+    return false;
   }
 }
