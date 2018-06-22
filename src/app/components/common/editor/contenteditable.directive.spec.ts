c62c0e5e1b62846e979b2658abd728523c6168b8
Philipp Tkachev <zoonman@gmail.com>
2018-06-22 15:34:07 -0400

    Fixing bugs. Modified contenteditable.directive.spec.ts

--- src/app/components/common/editor/contenteditable.directive.spec.ts
+++ src/app/components/common/editor/contenteditable.directive.spec.ts
@@ -1,8 +1,10 @@
 import { ContenteditableDirective } from './contenteditable.directive';
+import { ElementRef } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 describe('ContenteditableDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ContenteditableDirective();
+  it('should create an instance', (private el, private sanitizer) => {
+    const directive = ContenteditableDirective(el, sanitizer);
     expect(directive).toBeTruthy();
   });
 });
