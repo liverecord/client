2b18967477e88ceb60ae7da3ec620d0e41ed4dce
Philipp Tkachev <zoonman@gmail.com>
2018-07-01 15:34:09 -0400

    Fixing bugs. Modified settings.component.ts

--- src/app/components/users/settings/settings.component.ts
+++ src/app/components/users/settings/settings.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../../models/user';
+import { UserService } from '../../../services/user.service';
+import { StorageService } from '../../../services/storage.service';
+import { Title } from '@angular/platform-browser';
+import { i18nExtract } from '@angular/compiler-cli/src/transformers/program';
 
 @Component({
   selector: 'lr-settings',
@@ -6,10 +11,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.styl']
 })
 export class SettingsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  profile: User;
+  sending: boolean;
+  constructor(private userService: UserService,
+              private storage: StorageService,
+              private titleService: Title) {
+    this.sending = false;
+    this.profile = new User();
   }
 
+  ngOnInit() {
+    this.titleService.setTitle('Settings');
+    this.userService
+      .getUser(true)
+      .subscribe(u => this.profile = u);
+  }
+
+  saveGlobalSettings() {
+    //
+  }
+
+  signOut() {
+    this.userService.signOut();
+  }
+
+  forgetDevice() {
+    this.storage.clear();
+  }
 }
