704d61531d0112c3f2a46dc8fadd0f55b33de298
Philipp Tkachev <zoonman@gmail.com>
2018-07-04 15:34:11 -0400

    Fixing bugs. Modified user.ts

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,10 +1,35 @@
 import {Model} from './model';
 
+export class Notificatons {
+  email: boolean;
+
+  public constructor() {
+    this.email = false;
+  }
+}
+
+export class Settings {
+  notifications: Notificatons;
+
+  public constructor() {
+    this.notifications = new Notificatons();
+  }
+}
+
 export class User extends Model {
   name: string;
   online: boolean;
   email?: string;
+  gender?: string;
   picture?: string;
   slug?: string;
   rank?: number;
+  settings: Settings;
+
+  public constructor() {
+    super();
+    this.settings = new Settings();
+  }
 }
+
+
